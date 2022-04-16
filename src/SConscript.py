20684a254ce272ed7e17f1585f2fcf2b01c47578
Alexandr <sasha_xarlamov@mail.ru>
2022-04-17 00:14:47 +0300

    decider update

--- src/SConscript.py
+++ src/SConscript.py
@@ -4,6 +4,7 @@ libpath=[r'C:\programs\lemon-debug\lib/', r'C:\Code\c++\SFML-2.5.1-windows-vc15-
 libs = ['sfml-graphics-d', 'sfml-system-d', 'sfml-window-d', 'lemon']
 ccflags = ['/std:c++17', '/MDd', '/EHs']
 env = Environment(CPPPATH=system_include + include, LIBPATH = libpath, LIBS=libs, CCFLAGS = ccflags, LINK = 'lld-link', CXX = 'clang-cl' )
+env.Decider('MD5-timestamp')
 env.Tool('compilation_db')
 env.CompilationDatabase()
 sources = Glob('*/*.cpp') + Glob('*.cpp')
